8cd0d570b09231fe9dafe3ec3524df50598dfdf2
jlarge1 <largejonathan@gmail.com>
2018-07-27 23:54:13 -0400

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
+INSERT INTO burgers (burger_name) VALUES ("Macaroni and Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Mushroom Burger");
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
