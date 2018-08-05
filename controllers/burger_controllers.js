f4b616ab24afeaa3bc94585e36b1290fe9f210fd
jlarge1 <largejonathan@gmail.com>
2018-08-04 21:11:23 -0400

    working app

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -17,7 +17,8 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burger", function (req, res) {
-  burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function (result) {
+  console.log("req.body: " + req.body.burger_name);
+  burger.create(["burger_name"], [req.body.burger_name], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -19,27 +19,26 @@ $(function() {
       );
     });
   
-    // $(".create-form").on("submit", function(event) {
-    //   // Make sure to preventDefault on a submit event.
-    //   event.preventDefault();
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
   
-    //   var newCat = {
-    //     name: $("#ca").val().trim(),
-    //     sleepy: $("[name=sleepy]:checked").val().trim()
-    //   };
+      var newBurger = {
+        burger_name: $("#burg").val().trim()
+      };
   
-    //   // Send the POST request.
-    //   $.ajax("/api/cats", {
-    //     type: "POST",
-    //     data: newCat
-    //   }).then(
-    //     function() {
-    //       console.log("created new cat");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+      // Send the POST request.
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
   
     $(".deleteBurger").on("click", function(event) {
       var id = $(this).data("id");
